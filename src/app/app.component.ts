70740c771ef9fc8ab74f83dd2f1a1b8383e880bf
gabriel-rusu <rusugabriel13@gmail.com>
2024-10-09 21:35:14 +0300

    added dark mode initial scratch + tested some tailwind concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NgOptimizedImage} from "@angular/common";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {EventComponent} from "./components/event/event.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, EventComponent],
+  imports: [RouterOutlet, NgOptimizedImage, EventComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'webpage-v3';
+
+  changeTheme() {
+    console.log('Here ?');
+    console.log(document.getElementsByTagName('html'))
+    const htmlTag = document.getElementsByTagName('html').item(0);
+    if (htmlTag?.classList.contains('dark')) {
+      htmlTag.classList.remove('dark')
+    } else if (htmlTag) {
+      htmlTag.classList.add('dark');
+    }
+  }
 }
--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
