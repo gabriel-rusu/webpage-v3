e8f86cc88d913be2ffe16676e3dae0f896c02749
gabriel-rusu <rusugabriel13@gmail.com>
2024-12-07 23:04:00 +0200

    updated the 'Contact me' form wo send the info to my email. work on #29

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import * as emailjs from "@emailjs/browser";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  fullName= signal<string>('');
+  message = signal<string>('');
+  email = signal<string>('');
 
+  sendEmail() {
+    const templateParams = {
+      full_name: this.fullName(),
+      message: this.message(),
+      email: this.email()
+    }
+    emailjs.init('3OBVYlu1jm4XBAIb6');
+    emailjs.send('service_rv6skes', 'template_vbmyzuh', templateParams).then(
+      (response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      },
+      (error) => {
+        console.log('FAILED...', error);
+      },
+    );
+  }
 }
