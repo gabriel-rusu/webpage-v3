b987889ce440d2e1f726b972c7d491266774edb3
gabriel-rusu <rusugabriel13@gmail.com>
2024-12-14 11:49:15 +0200

    Now the user selected theme will be persisted in local storage. closes #33

--- src/app/components/theme-selector/theme-selector.component.ts
+++ src/app/components/theme-selector/theme-selector.component.ts
@@ -1,17 +1,28 @@
-import {Component, HostBinding, Input, ViewChild} from '@angular/core';
+import {Component, HostBinding, Input, OnInit, ViewChild} from '@angular/core';
 import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-theme-selector',
   standalone: true,
   templateUrl: './theme-selector.component.html',
-  styleUrl: './theme-selector.component.css'
+  styleUrl: './theme-selector.component.css',
+  host :{
+    'class': 'absolute right-4 top-4'
+  }
 })
-export class ThemeSelectorComponent {
-  @HostBinding('class') classes = 'absolute right-4 top-4'
+export class ThemeSelectorComponent implements OnInit {
   isDarkTheme: boolean = true;
   isDarkMode: boolean = false;
   @ViewChild('toggle') toggle: any;
+
+  ngOnInit(): void {
+    const userSelectedTheme = localStorage.getItem('theme')
+    if (userSelectedTheme != null && userSelectedTheme == 'light') {
+      setTimeout(() => {
+        this.changeMode();
+      }, 10);
+    }
+  }
 
 
   changeTheme() {
@@ -21,9 +32,11 @@ export class ThemeSelectorComponent {
     if (htmlTag?.classList.contains('dark')) {
       htmlTag.classList.remove('dark')
       this.isDarkTheme = false;
+      localStorage.setItem('theme', 'light');
     } else if (htmlTag) {
       htmlTag.classList.add('dark');
       this.isDarkTheme = true;
+      localStorage.setItem('theme', 'dark');
     }
   }
 
